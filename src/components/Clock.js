30e66aab6361da80092137c1fdf1d96e6ad86f8a
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-12-14 02:08:24 +0600

    clock in bn

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 class Clock extends Component {
+  state = {
+    date: new Date(),
+  };
+
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
   render() {
-    return <div>
-      <h1>
-        <span>{new Date().toLocaleTimeString(this.props.local)}</span>
-      </h1>
-    </div>;
+    return (
+      <div>
+        <h1>
+          <span>{this.state.date.toLocaleTimeString(this.props.local)}</span>
+        </h1>
+      </div>
+    );
   }
 }
 
